26c64538a8b98d2b9a8aa44ad43e45f9eb3721ef
qudsia99 <136015250+qudsia99@users.noreply.github.com>
2024-02-08 22:49:07 -0500

    removed clutter

--- Deployment/static/bootstrap.js
+++ Deployment/static/bootstrap.js
@@ -26,3 +26,29 @@ function nextPage(){
   // if path doesn't work TRY: 'templates/next-page.html'
 
 }
+
+function homePage(){
+  window.location.href = '/';
+
+}
+
+
+function searchFunction() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("search");
+  filter = input.value.toUpperCase();
+  table = document.querySelector("table");
+  tr = table.getElementsByTagName("tr");
+
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
